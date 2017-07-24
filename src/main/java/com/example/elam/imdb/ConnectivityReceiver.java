c030f62e49d0f49c03092090891f6d53d8a6f00e
Navinbala5 <bnavin28@gmail.com>
2017-07-24 15:49:16 +0530

    Add files via upload

---
+++ src/main/java/com/example/elam/imdb/ConnectivityReceiver.java
@@ -0,0 +1,49 @@
+package com.example.elam.imdb;
+
+/**
+ * Created by elam on 24-07-2017.
+ */
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class ConnectivityReceiver extends BroadcastReceiver {
+    //  Context mContext;
+
+    public static ConnectivityReceiverListener connectivityReceiverListener;
+
+    public ConnectivityReceiver() {
+        super();
+    }
+
+    @Override
+    public void onReceive(Context mContext, Intent arg1) {
+        ConnectivityManager cm = (ConnectivityManager) mContext
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null
+                && activeNetwork.isConnectedOrConnecting();
+
+        if (connectivityReceiverListener != null) {
+            connectivityReceiverListener.onNetworkConnectionChanged(isConnected);
+        }
+    }
+
+    public static boolean isConnected() {
+
+        ConnectivityManager
+                cm = (ConnectivityManager) MyApplication.getInstance().getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null
+                && activeNetwork.isConnectedOrConnecting();
+    }
+
+
+    public interface ConnectivityReceiverListener {
+        void onNetworkConnectionChanged(boolean isConnected);
+    }
+}
---
+++ src/main/java/com/example/elam/imdb/MyApplication.java
@@ -0,0 +1,27 @@
+package com.example.elam.imdb;
+
+/**
+ * Created by elam on 24-07-2017.
+ */
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    private static MyApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mInstance = this;
+    }
+
+    public static synchronized MyApplication getInstance() {
+        return mInstance;
+    }
+
+    public void setConnectivityListener(ConnectivityReceiver.ConnectivityReceiverListener listener) {
+        ConnectivityReceiver.connectivityReceiverListener = listener;
+    }
+}
